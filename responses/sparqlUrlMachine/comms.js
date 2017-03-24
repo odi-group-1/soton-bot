ea2f9d30e16cdc16a05a77d17c4af8c5129e2cdc
stefanjcollier <stefanjcollier@hotmail.com>
2017-03-24 16:02:42 +0000

    return all services

--- responses/sparqlUrlMachine/comms.js
+++ responses/sparqlUrlMachine/comms.js
@@ -3,9 +3,12 @@ const request = require('request');
 
 let get = (url) => {
     request(url, function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
+        if (error){
+            console.log('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            console.log('body:', body); // Print the HTML for the Google homepage.
+        }
+        return body.results.bindings;
     });
 };
 
