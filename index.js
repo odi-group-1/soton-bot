a18b43623cb208b89202f495124b10cac1366789
Deepak Thankachan <deepakthankachan@Deepaks-MacBook-Pro.local>
2017-04-27 13:31:39 +0100

    added bus route query

--- index.js
+++ index.js
@@ -52,22 +52,14 @@ app.get('/parser/', (req, res) => {
 
 app.get('/tom/', (req, res) => {
 
-    let d = new Date().toISOString().split('.')[0]+"Z";
-
-    let dat = d.split('T')[0] + 'T';
-    let nowHr = d.split('T')[1].split(':')[0];
-    nowHr = (nowHr + 1 <= 24) ? nowHr + 1 : nowHr;
-
-    let dateSt = dat + '00:00:00Z';
-    let dateEnd = dat + '23:59:59Z';
-    let dateNow = dat + nowHr + ':00:00Z';
-
-
     let stored = require('./service/sparqlUrlMachine/storedQueries');
-    let queryJson = stored.freeRoom(dateSt,dateEnd,dateNow);
-
+    let queryJson = stored.busRoutes('1980SN120415','1980SN120415');
     let jqc = require('./service/sparqlUrlMachine/jsonQueryConverter');
     jqc.getOfferings(queryJson, function (allOfferings) {
+
+
+        console.log("hey hey hey ");
+
         res.send(allOfferings);
 
     },function (error) {
