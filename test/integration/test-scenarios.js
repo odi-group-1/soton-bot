feaa2248dccaa32b4be0fb470401dcd2c4c6b076
stefanjcollier <stefanjcollier@hotmail.com>
2017-05-01 12:31:11 +0100

    apiai int' tests: offering, room detail, book room

--- test/integration/test-scenarios.js
+++ test/integration/test-scenarios.js
@@ -10,7 +10,7 @@ let tests = [
         }
     },{
         message: 'Where is building 32?',
-        sessionId: '1001',
+        sessionId: '1100',
         result: {
             'action': 'find-building',
             'actionIncomplete': false,
@@ -21,7 +21,7 @@ let tests = [
         }
     },{
         message: 'Where is building?',
-        sessionId: '1002',
+        sessionId: '1100',
         result: {
             'action': 'find-building',
             'actionIncomplete': true,
@@ -43,7 +43,7 @@ let tests = [
         }
     },{
         message: 'Where can I get condoms?',
-        sessionId: '1003',
+        sessionId: '1200',
         result: {
             'action': 'find-nearest-service',
             'actionIncomplete': true,
@@ -64,5 +64,93 @@ let tests = [
 
             }
         }
+    },{
+        message: 'Where can I get booze?',
+        sessionId: '1201',
+        result: {
+            'action': 'find-nearest-service',
+            'actionIncomplete': true,
+            'parameters': {
+                'hidden-found-location-key': '',
+                'offering': 'Alcohol'
+            },
+        },
+        followUp: {
+            message: 'got-coords--InaDeepakTomShakibStefan-hidden-key',
+            result: {
+                'action': 'find-nearest-service',
+                'actionIncomplete': false,
+                'parameters': {
+                    'hidden-found-location-key': 'got-coords--InaDeepakTomShakibStefan',
+                    'offering': 'Alcohol'
+                }
+
+            }
+        }
+    },{
+        message: 'Where can I get coffee?',
+        sessionId: '1202',
+        result: {
+            'action': 'find-nearest-service',
+            'actionIncomplete': true,
+            'parameters': {
+                'hidden-found-location-key': '',
+                'offering': 'Caffeine'
+            },
+        },
+        followUp: {
+            message: 'got-coords--InaDeepakTomShakibStefan-hidden-key',
+            result: {
+                'action': 'find-nearest-service',
+                'actionIncomplete': false,
+                'parameters': {
+                    'hidden-found-location-key': 'got-coords--InaDeepakTomShakibStefan',
+                    'offering': 'Caffeine'
+                }
+            }
+        }
+    },{
+        message: 'Tell me about building 32 room 3001?',
+        sessionId: '1300',
+        result: {
+            'action': 'find-room-details',
+            'actionIncomplete': false,
+            'parameters': {
+                'building': '32',
+                'room': '3001'
+            },
+        }
+    },{
+        message: 'Where can I grab a room on 21st July 2018?',
+        sessionId: '1400',
+        result: {
+            'action': 'find-bookable-rooms',
+            'actionIncomplete': false,
+            'parameters': {
+                'bookingTime': '2018-07-21'
+            },
+        }
+    },{
+        message: 'Where can I grab a room?',
+        sessionId: '1401',
+        result: {
+            'action': 'find-bookable-rooms',
+            'actionIncomplete': true,
+            'parameters': {
+                'bookingTime': ''
+            },
+        },
+        followUp: {
+            message: '21st July 2018?',
+            result: {
+                'action': 'find-bookable-rooms',
+                'actionIncomplete': false,
+                'parameters': {
+                    'bookingTime': '2018-07-21'
+                },
+            }
+        }
+    },{
+
     }
 ];
