9193e9991728e46ee8ab5dcce986880400368f79
shakib bin hamid <sh3g12@soton.ac.uk>
2017-05-18 18:22:18 +0100

    removed our api keys

--- config/staging.js
+++ config/staging.js
@@ -5,11 +5,11 @@
 module.exports = {
     PORT : 5000,
     APP_TOKEN : 'we-will-change-student-lives-in-soton',
-    API_AI_CLIENT_ID : '0131221ffe7b41b49872ed7e12b53237',
+    API_AI_CLIENT_ID : 'your api.ai client id',
     API_AI_HIDDEN_KEYS : {
         COORDINATE: 'got-coords--InaDeepakTomShakibStefan-hidden-key'
     },
     TRANSPORT_API_NEAR_ENDPOINT: 'https://transportapi.com/v3/uk/bus/stops/near.json?',
-    TRANSPORT_API_APP_ID: '080d8e06',
-    TRANSPORT_API_APP_KEY: '3c0e2a0258d9f338ffe9f449574222ee'
+    TRANSPORT_API_APP_ID: 'your transport api app id',
+    TRANSPORT_API_APP_KEY: 'your transport api app key'
 };
